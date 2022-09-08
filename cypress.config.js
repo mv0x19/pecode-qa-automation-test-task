6ddfdef53da97da10852df0e0191821d00b3597e
mv0x19 <49604532+mv0x19@users.noreply.github.com>
2022-09-09 01:14:02 +0300

    test: add base url and env credentials values

--- cypress.config.js
+++ cypress.config.js
@@ -2,8 +2,13 @@ const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
+    baseUrl:
+      'https://www.pecodesoftware.com/qa-portal/registerlogin/registerlogin.php',
+  },
+  env: {
+    user: {
+      username: 'username',
+      password: 'password',
     },
   },
 });
